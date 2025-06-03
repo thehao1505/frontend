f7c6be0361543fcc3aa97d544efe836201be92bc
The Hao <nt.hao@tego.global>
2025-06-03 16:16:36 +0700

    feat: add axios interceptor

---
+++ src/lib/axios.ts
@@ -0,0 +1,34 @@
+"use client";
+
+import axios from "axios";
+import { destroyCookie, parseCookies } from "nookies";
+
+const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = parseCookies().token;
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      destroyCookie(null, "token");
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
---
+++ src/middleware.ts
@@ -0,0 +1,38 @@
+import { jwtDecode } from "jwt-decode";
+import { NextRequest, NextResponse } from "next/server";
+import { destroyCookie } from "nookies";
+
+async function verifyToken(token: string) {
+  try {
+    const decoded_token = jwtDecode(token) as { exp: number };
+    const currentTime = Date.now() / 1000;
+    if (decoded_token.exp < currentTime) {
+      console.log("Expired token!");
+      return null;
+    }
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+export async function middleware(request: NextRequest) {
+  const token = request.cookies.get("token")?.value;
+  const path = request.nextUrl.pathname;
+
+  const protectedRoutes = ["/", "/messsages", "/notifications"];
+  const authRoutes = ["/login", "/sign-up", "/reset-password", "/forgot-password"];
+
+  const isAuthenticated = token && (await verifyToken(token));
+
+  if (!isAuthenticated && protectedRoutes.includes(path)) {
+    destroyCookie({ res: NextRequest }, "token", { path: "/" });
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  if (isAuthenticated && authRoutes.includes(path)) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  return NextResponse.next();
+}
